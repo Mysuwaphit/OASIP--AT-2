7b1b4e39fcae1b4c40993ed351f64a6b426a5f28
pafa212 <ninja212@hotmail.co.th>
2022-09-17 01:00:31 +0700

    submit 10th

--- Frontend/src/views/login.vue
+++ Frontend/src/views/login.vue
@@ -1,159 +1,149 @@
 <script setup>
-import { ref,onBeforeMount } from 'vue';
-import { useRouter } from 'vue-router' 
-import StatusBox from '../components/statusBox.vue';
-
-const appRouter = useRouter()
-const goToAddUser = () => appRouter.push({ name: 'AddUser' })
-const goToHome = () => appRouter.push({ name: 'EventListing' })
-const yourEmail = ref('')
-const yourPassword = ref('')
-let checkedUser = ref(false)
-let status = ref(0)
-
-const userList = ref([])
-const getUserList = async () => {
-  const res = await fetch(`${import.meta.env.VITE_BASE_URL}/users`)
-  if (res.status === 200) {
-    userList.value = await res.json(); 
-  } else {
-    console.log("No Category");
-  }
-};
-onBeforeMount(async () => {
-  await getUserList();
-
-});
-
-let validatedEmail = ref('')
-const validateEmail = () => {
-  const  validEmail = /^[a-zA-Z0-9.!#$%&*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  // yourEmail.value.length > 50? alert("Your name is too long. It's must at least 1 character and no more than 100 character.") : (yourEmail.value = yourEmail.value.trim())
-  return yourEmail.value.match(validEmail)? validatedEmail.value = yourEmail.value.trim() : alert("Please insert your email again. your email is not valid.") 
-}
-
-const checkUser = async (yourInputEmail,yourInputPassword) => {
-
-  if(yourInputEmail != '' && yourInputPassword != ''){
-    checkedUser.value = true
-    console.log(checkedUser.value);
-    const res = await fetch(`${import.meta.env.VITE_BASE_URL}/users/login`,{
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(
-        {
-          email: yourInputEmail,
-          userpassword: yourInputPassword
-        }
-      )
-    })
-    
-    if (res.status === 201 && res.status !== 400) {
-      const add = await res.json()
-      userList.value.push(add)
+  import { ref,onBeforeMount } from 'vue';
+  import { useRouter } from 'vue-router' 
+  import StatusBox from '../components/statusBox.vue';
+  const appRouter = useRouter()
+  const goToAddUser = () => appRouter.push({ name: 'AddUser' })
+  const goToHome = () => appRouter.push({ name: 'EventListing' })
+  const yourEmail = ref('')
+  const yourPassword = ref('')
+  let checkedUser = ref(false)
+  let status = ref(0)
+  let token = ref('')
+  const userList = ref([])
+  const getUserList = async () => {
+    const res = await fetch(`${import.meta.env.VITE_BASE_URL}/users`)
+    if (res.status === 200) {
+      userList.value = await res.json(); 
+    } else {
+      console.log("No Category");
     }
-    status.value = res.status
-    console.log(status.value);
-  }else alert('Password cannot be null')
-}
-</script>
- 
-<template>
-<!-- @edit="(i) => checkedUser = i" @success="goToHome" -->
-  <StatusBox v-if="checkedUser === true" :status="status" />
-  <div class="loginbox" id="demo-1">
-      <img class="logo" src="../assets/businessman.png"/>
-        <h1>Sign In</h1>
-        <form>
-            <label>Email</label>
-            <input type="email" placeholder="Enter Email" v-model="yourEmail" @focusout ="validateEmail" required>
-            <label>Password</label>     
-            <input type="password" placeholder="Enter Password" minlength="8" maxlength="14" v-model="yourPassword" required>
-            <button type="button" class="btn btn-primary trigger-btn" id="check" data-toggle="modal" data-target="#demo-2" data-dismiss="modal"  @click="checkUser(validatedEmail,yourPassword)">submit</button>
-        </form>
-        <p @click="goToAddUser">Didn't have account?</p>
-  </div>
-</template>
-
- 
-<style scoped>
-.close {
-	position: absolute;
-	top: 5px;
-	right: 2px;
-}
-.loginbox{
-    width: 400px;
-    height: 550px;
-    background: #1c2833;
-    color: white;
-    margin: 170px auto;
-    margin-left: 700px;
-    border-radius: 10px;
+  };
+  onBeforeMount(async () => {
+    await getUserList();
+  });
+  let validatedEmail = ref('')
+  const validateEmail = () => {
+    const  validEmail = /^[a-zA-Z0-9.!#$%&*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    // yourEmail.value.length > 50? alert("Your name is too long. It's must at least 1 character and no more than 100 character.") : (yourEmail.value = yourEmail.value.trim())
+    return yourEmail.value.match(validEmail)? validatedEmail.value = yourEmail.value.trim() : alert("Please insert your email again. your email is not valid.") 
+  }
+  const checkUser = async (yourInputEmail,yourInputPassword) => {
+    if(yourInputEmail != '' && yourInputPassword != ''){
+      checkedUser.value = true
+      console.log(checkedUser.value);
+      const res = await fetch(`${import.meta.env.VITE_BASE_URL}/users/login`,{
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(
+          {
+            email: yourInputEmail,
+            userpassword: yourInputPassword
+          }
+        )
+      })
+      
+      if (res.status === 201 && res.status !== 400) {
+        const add = await res.json()
+        userList.value.push(add)
+        token.value = await res.body.token
+      }
+      status.value = res.status
+      console.log(status.value);
+      console.log(res.headers);
+      console.log(res.body);
+      console.log(res.token);
+    }else alert('Password cannot be null')
+  }
+  </script>
+   
+  <template>
+  <!-- @edit="(i) => checkedUser = i" @success="goToHome" -->
+    <StatusBox v-if="checkedUser === true" :status="status" />
+    <div class="loginbox" id="demo-1">
+        <img class="logo" src="../assets/businessman.png"/>
+          <h1>Sign In</h1>
+          <form>
+              <label>Email</label>
+              <input type="email" placeholder="Enter Email" v-model="yourEmail" @focusout ="validateEmail" required>
+              <label>Password</label>     
+              <input type="password" placeholder="Enter Password" minlength="8" maxlength="14" v-model="yourPassword" required>
+              <button type="button" class="btn btn-primary trigger-btn" id="check" data-toggle="modal" data-target="#demo-2" data-dismiss="modal"  @click="checkUser(validatedEmail,yourPassword)">submit</button>
+          </form>
+          <p @click="goToAddUser">Didn't have account?</p>
+    </div>
+  </template>
+   
+  <style scoped>
+  .close {
     position: absolute;
-    padding-top: 200px;
-    padding: 50px;
-}
-.logo{
-    margin-top: 30px;
-    width: 100px;
-    height:100px;
-    position: relative;
-    top: -70px;
-    left: calc(50% - 50px);
-    z-index: 0;
-}
-h1{
-    text-align: center;
-    margin-top: -60px;
-}
-
-label{
-
-    font-size: 18px;
-    display: block;
-    margin-left: 20px;
-}
-
-input{
-
-    width: 80%;
-    margin: 0 0 20px 20px;
-}
-
-input[type="email"],input[type="password"]{
-    border: none;
-    background: transparent;
-    border-bottom: 1px solid #fff;
-    height: 40px;
-    outline: none;
-    color: white;
-    font-size: 16px;
-}
-
-#check{
-    margin-top: 20px;
-    border: none;
-    outline: none;
-    height: 40px;
-    width: 300px;
-    border-radius: 20px;
-    background: #fb2525;
-    color: white;
-    font-size: 20px;
-    cursor: pointer;
-}
-
-p{
-    margin-top: 20px;
-    text-decoration: none;
-    color: white;
-    margin-left: 25%;
-}
-
-#check:hover{
-    background: blueviolet;
-}
-</style>
+    top: 5px;
+    right: 2px;
+  }
+  .loginbox{
+      width: 400px;
+      height: 550px;
+      background: #1c2833;
+      color: white;
+      margin: 170px auto;
+      margin-left: 700px;
+      border-radius: 10px;
+      position: absolute;
+      padding-top: 200px;
+      padding: 50px;
+  }
+  .logo{
+      margin-top: 30px;
+      width: 100px;
+      height:100px;
+      position: relative;
+      top: -70px;
+      left: calc(50% - 50px);
+      z-index: 0;
+  }
+  h1{
+      text-align: center;
+      margin-top: -60px;
+  }
+  label{
+      font-size: 18px;
+      display: block;
+      margin-left: 20px;
+  }
+  input{
+      width: 80%;
+      margin: 0 0 20px 20px;
+  }
+  input[type="email"],input[type="password"]{
+      border: none;
+      background: transparent;
+      border-bottom: 1px solid #fff;
+      height: 40px;
+      outline: none;
+      color: white;
+      font-size: 16px;
+  }
+  #check{
+      margin-top: 20px;
+      border: none;
+      outline: none;
+      height: 40px;
+      width: 300px;
+      border-radius: 20px;
+      background: #fb2525;
+      color: white;
+      font-size: 20px;
+      cursor: pointer;
+  }
+  p{
+      margin-top: 20px;
+      text-decoration: none;
+      color: white;
+      margin-left: 25%;
+  }
+  #check:hover{
+      background: blueviolet;
+  }
+  </style>
